2edca60664b66cb234f769281473d48ca10cb6fa
Alexis <alexismuguetpro@gmail.com>
2022-03-15 17:00:10 +0100

    :rocket: Connexion du front au back

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import uuid from 'react-native-uuid';
+import axios from 'axios';
+import { format } from 'date-fns';
 
 export const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      uuid: uuid.v4(),
-      title: 'Sortir les poubelles',
-      description: 'Poubelle jaune le mardi et grise le jeudi',
-      priority: '1'
-    }, {
-      uuid: uuid.v4(),
-      title: 'Faire le plein',
-      description: 'Utiliser l\'indemnité inflation de 100€',
-      date: '2021-04-02',
-      isDone: false,
-      priority: '3'
-    }, {
-      uuid: uuid.v4(),
-      title: 'Arroser les plantes',
-      description: 'Surtout les géraniums',
-      date: '2021-03-17',
-      isDone: false,
-      priority: '1'
-    }, {
-      uuid: uuid.v4(),
-      title: 'Confirmer la location du canoë',
-      description: 'Prévoir 12€/personne pour la demi-journée',
-      date: '2021-03-20',
-      isDone: false,
-      priority: '2'
-    }
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/todos')
+      .then((res) => {
+        setTasks(res.data);
+      });
+  }, []);
 
   const [newTask, setNewTask] = useState({
-    uuid: uuid.v4(), title: '', description: '', date: new Date().toISOString().split('T')[0]
+    uuid: uuid.v4(), titre: '', description: '', date: new Date().toISOString().split('T')[0]
   });
   const [isEditing, setIsEditing] = useState(false);
 
   const emptyForm = {
-    uuid: uuid.v4(), title: '', description: '', priority: '', date: new Date().toISOString().split('T')[0]
+    uuid: uuid.v4(), titre: '', description: '', priorite: '', date: new Date().toISOString().split('T')[0]
   };
 
   const changeSelectedIndex = (selectValue) => {
-    document.querySelector('#priority-select').value = selectValue;
+    document.querySelector('#priorite-select').value = selectValue;
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setTasks([...tasks, newTask]);
-    setNewTask(emptyForm);
-    changeSelectedIndex('');
+
+    axios.post('http://localhost:5000/api/todos', {
+      uuid: uuid.v4(),
+      titre: newTask.titre,
+      description: newTask.description,
+      priorite: parseInt(newTask.priorite, 10),
+      date: newTask.date
+    }).then(() => {
+      setTasks([...tasks, newTask]);
+      setNewTask(emptyForm);
+      changeSelectedIndex('');
+    })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   const handleChange = (e) => {
@@ -58,14 +51,19 @@ export const App = () => {
   };
 
   const handleRemove = (id) => {
-    const newList = tasks.filter((task) => task.uuid !== id);
-    setTasks(newList);
+    axios.delete(`http://localhost:5000/api/todos/${id}`).then(() => {
+      const newList = tasks.filter((task) => task.uuid !== id);
+      setTasks(newList);
+    })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   const handleEdit = (task) => {
     setNewTask(task);
     setIsEditing(true);
-    changeSelectedIndex(task.priority);
+    changeSelectedIndex(task.priorite);
   };
 
   const handleCancel = (e) => {
@@ -77,30 +75,57 @@ export const App = () => {
 
   const handleEditSubmit = (e) => {
     e.preventDefault();
-    const newList = tasks.map((task) => (task.uuid === newTask.uuid ? newTask : task));
-    setTasks(newList);
-    setNewTask(emptyForm);
-    changeSelectedIndex('');
-    setIsEditing(false);
+
+    axios.put(`http://localhost:5000/api/todos/${newTask.uuid}`, {
+      titre: newTask.titre,
+      description: newTask.description,
+      priorite: parseInt(newTask.priorite, 10),
+      date: newTask.date
+    }).then(() => {
+      const newList = tasks.map((task) => (task.uuid === newTask.uuid ? newTask : task));
+      setTasks(newList);
+      setNewTask(emptyForm);
+      changeSelectedIndex('');
+      setIsEditing(false);
+    })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   const doneTask = (e) => {
     const element = e.target.parentElement;
-    element.classList.toggle('crossed-line');
+    let boolIsDone = false;
+
     const newList = tasks.map(
-      (task) => (task.uuid === e.target.id ? { ...task, isDone: !task.isDone } : task)
+      (task) => {
+        if (task.uuid === e.target.id) {
+          boolIsDone = !task.isDone;
+          return { ...task, isDone: boolIsDone };
+        }
+        return task;
+      }
     );
-    setTasks(newList);
+
+    axios.put(`http://localhost:5000/api/todos/${e.target.id}`, {
+      isDone: boolIsDone
+    }).then(() => {
+      element.classList.toggle('crossed-line');
+      setTasks(newList);
+    })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   const sortTasks = (e) => {
     const sortElement = e.target.value;
-    if (sortElement === 'title') {
-      tasks.sort((a, b) => ((a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0)));
+    if (sortElement === 'titre') {
+      tasks.sort((a, b) => ((a.titre > b.titre) ? 1 : ((b.titre > a.titre) ? -1 : 0)));
     } else if (sortElement === 'date') {
       tasks.sort((a, b) => ((a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0)));
     } else {
-      tasks.sort((a, b) => ((a.priority < b.priority) ? 1 : ((b.priority < a.priority) ? -1 : 0)));
+      tasks.sort((a, b) => ((a.priorite < b.priorite) ? 1 : ((b.priorite < a.priorite) ? -1 : 0)));
     }
     setTasks([...tasks]);
   };
@@ -114,14 +139,14 @@ export const App = () => {
           <h1>Nouvelle tâche</h1>
         )}
         <form method="GET" onSubmit={handleSubmit}>
-          <label htmlFor="titleTask"> Nom de la tâche : </label>
-          <input id="titleTask" type="text" name="title" value={newTask.title} onChange={handleChange} />
+          <label htmlFor="titreTask"> Nom de la tâche : </label>
+          <input id="titreTask" type="text" name="titre" value={newTask.titre} onChange={handleChange} />
           <br />
           <label htmlFor="descriptionTask"> Description : </label>
           <input id="descriptionTask" type="textarea" name="description" value={newTask.description} onChange={handleChange} />
           <br />
-          <label htmlFor="priority-select"> Priorité : </label>
-          <select name="priority" id="priority-select" onChange={handleChange}>
+          <label htmlFor="priorite-select"> Priorité : </label>
+          <select name="priorite" id="priorite-select" onChange={handleChange}>
             <option value="">Aucune</option>
             <option value="3">Urgente</option>
             <option value="2">Haute</option>
@@ -130,7 +155,7 @@ export const App = () => {
           </select>
           <br />
           <label htmlFor="dateTask"> À faire pour le : </label>
-          <input id="dateTask" type="date" name="date" value={newTask.date} onChange={handleChange} />
+          <input id="dateTask" type="date" name="date" value={format(new Date(newTask.date), 'yyyy-MM-dd')} onChange={handleChange} />
           <br />
           {isEditing ? (
             <div>
@@ -145,12 +170,12 @@ export const App = () => {
 
       <ul>
         {tasks.map((task) => (
-          <li key={task.uuid}>
-            <input type="checkbox" id={task.uuid} onClick={doneTask} />
+          <li key={task.uuid} className={task.isDone ? 'crossed-line' : ''}>
+            <input type="checkbox" id={task.uuid} onClick={doneTask} defaultChecked={task.isDone ? 'checked' : ''} />
             <label htmlFor={task.uuid}>
-              {task.title}({task.description})
-              <strong>Pour le :</strong>{task.date}
-              <strong>Priorité :</strong>{task.priority}
+              {task.titre} ({task.description})
+              <br /><strong>Pour le : </strong>{format(new Date(task.date), 'dd/MM/yyyy')}
+              <br /><strong>Priorité : </strong>{task.priorite}
             </label>
             <button type="button" onClick={() => handleRemove(task.uuid)}>Supprimer</button>
             <button type="button" onClick={() => handleEdit(task)}>Modifier</button>
@@ -159,10 +184,10 @@ export const App = () => {
       </ul>
       <div>
         <h2>Trier par :</h2>
-        <input type="radio" name="tri" id="sort-title" value="title" onClick={sortTasks} />
-        <label htmlFor="sort-title">Titre</label>
-        <input type="radio" name="tri" id="sort-priority" value="priority" onClick={sortTasks} />
-        <label htmlFor="sort-priority">Priorité</label>
+        <input type="radio" name="tri" id="sort-titre" value="titre" onClick={sortTasks} />
+        <label htmlFor="sort-titre">Titre</label>
+        <input type="radio" name="tri" id="sort-priorite" value="priorite" onClick={sortTasks} />
+        <label htmlFor="sort-priorite">Priorité</label>
         <input type="radio" name="tri" id="sort-date" value="date" onClick={sortTasks} />
         <label htmlFor="sort-date">Date</label>
       </div>
