06f31402dfedde87e82f1578a826a102c7dd0e9d
Alexis <alexismuguetpro@gmail.com>
2022-03-15 12:00:51 +0100

    :lock: Ajout de CORS

--- server/App.js
+++ server/App.js
@@ -1,11 +1,12 @@
 const { PrismaClient } = require('@prisma/client');
 const express = require('express');
+const cors = require('cors');
 
-const port = 3000;
+const port = 5000;
 const app = express();
 const prisma = new PrismaClient();
 
-app.use(express.json());
+app.use(express.json(), cors());
 
 app.route('/api/todos')
   .get((req, res) => {
